ee1e69bf46b76c300b271ab4337d9a38b8c8543e
wuliya <3369906077@qq.com>
2025-04-10 18:09:59 +0800

    ci: 更新发布流程以使用 GitHub App 令牌

---
+++ lib/apps/search.d.ts
@@ -0,0 +1,1 @@
+export declare const search: import("node-karin").Command<"message">;
---
+++ lib/models/help/config.d.ts
@@ -0,0 +1,2 @@
+import type { HelpType } from '../../types/index.js';
+export declare const helpCfg: HelpType['helpCfg'];
